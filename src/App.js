1f68241ddb007107c3cdc48d0a9c580b00d4c770
Aayush <aayush5emails@gmail.com>
2022-05-10 20:54:23 +0530

    frontend complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import { Button } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import Card from "./Components/Card";
 import "./app.scss";
 
 function App() {
   return (
-    <div className="App">
-      <Card>
-        <div className="UpperPart">
-          <Button variant="contained">Connect to Google Drive</Button>
-          <Button variant="contained">Select File From Google Drive</Button>
-        </div>
-      </Card>
+    <div className="overlay">
+      <div className="Model">
+        <Card>
+          <div className="UpperPart">
+            <Button variant="contained">Connect to Google Drive</Button>
+            <Button variant="contained">Select File From Google Drive</Button>
+          </div>
+          <div className="LowerPart">
+            <Typography>Selected File...</Typography>
+            <Box className="ItemsBox">
+              <Typography>Hello</Typography>
+              <Typography>Hello</Typography>
+              <Typography>Hello</Typography>
+              <Typography>Hello</Typography>
+              <Typography>Hello</Typography>
+              <Typography>Hello</Typography>
+            </Box>
+          </div>
+        </Card>
+      </div>
     </div>
   );
 }
